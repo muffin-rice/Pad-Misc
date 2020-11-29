427f83308546a30a27aa41800786f36f85b6a584
muffin-rice <davidluzhu@gmail.com>
2020-11-29 01:28:14 -0800

    changed skyfall stuff

--- combo_sim.py
+++ combo_sim.py
@@ -7,7 +7,7 @@ from match import Match
 
 columns = 6
 rows = 5
-ITERATIONS = 10000
+ITERATIONS = 100000
 sf_distribution = [1 / 6, 1 / 6, 1 / 6, 1 / 6, 1 / 6, 1 / 6, 0, 0]
 
 
@@ -26,12 +26,12 @@ def find_combos(board: [[str]]):
             if prev_letter == row[j]:
                 counter += 1
             else:
-                if counter >= 3:
+                if counter >= 3 and prev_letter != ' ':
                     all_combos.append(Match((i, j - counter), counter, 'h', prev_letter))
                 prev_letter = row[j]
                 counter = 1
 
-            if j == columns - 1 and counter >= 3:
+            if j == columns - 1 and counter >= 3 and prev_letter != ' ':
                 all_combos.append(Match((i, j - counter + 1), counter, 'h', prev_letter))
 
     # then go vertical
@@ -42,12 +42,12 @@ def find_combos(board: [[str]]):
             if prev_letter == board[i][j]:
                 counter += 1
             else:
-                if counter >= 3:
+                if counter >= 3 and prev_letter != ' ':
                     all_combos.append(Match((i - counter, j), counter, 'v', prev_letter))
                 prev_letter = board[i][j]
                 counter = 1
 
-            if i == rows - 1 and counter >= 3:
+            if i == rows - 1 and counter >= 3 and prev_letter != ' ':
                 all_combos.append(Match((i - counter + 1, j), counter, 'v', prev_letter))
 
     # see if they overlap using __contains__ dunder in Match class
@@ -66,7 +66,6 @@ def find_combos(board: [[str]]):
 
 
 def fall_all(board: [[str]]):
-
     for j in range(columns):
         col = [board[rows-i-1][j] for i in range(rows) if board[rows-i-1][j] != ' ']
         for i in range(rows):
@@ -75,140 +74,77 @@ def fall_all(board: [[str]]):
             else:
                 board[rows-i-1][j] = ' '
 
+def create_skyfalls(board: [[str]]):
+    for i in range(rows):
+        for j in range(columns):
+            if board[i][j] == ' ':
+                board[i][j] = random.choices('RBGLDHJP', sf_distribution)[0]
 
-def remove_combos(board: [[str]], combos: [[(int, int)]], skyfall=True):
+def remove_combos(board: [[str]], combos: [[(int, int)]]):
     #replaces coordinates in matches with blanks
     for combo in combos:
         for coord in combo:
-            board[coord[0]][coord[1]] = ' '  # just using " " to represent the board 
+            board[coord[0]][coord[1]] = ' '
 
-    fall_all(board)
+def combo_sim(board_string: str, x_sf: (int, int), print_stats=True, skyfall=True) -> \
+        (str, int, int, int, int):
+    '''board size is 0 for 6x5 and 1 for 7x6;
+    x_sf is how many skyfalls you want over how many turns'''
+    assert len(board_string) == rows * columns
 
-    if skyfall:
-        for i in range(rows):
-            for j in range(columns):
-                if board[i][j] == ' ':
-                    board[i][j] = random.choices('RBGLDHJP', sf_distribution)[0]
+    perm_board = [[board_string[i * columns + j] for j in range(columns)] for i in range(rows)]
 
+    # no skyfall combo count is on cc_list[0]
 
-def find_nosf_combos(board: [[str]]):
-    all_combos = []
-    # first go horizontal
-    for i, row in enumerate(board):
-        counter = 1
-        prev_letter = '*'
-        for j in range(columns):
-            if row[j] == ' ':
-                continue
-            if prev_letter == row[j]:
-                counter += 1
-            else:
-                if counter >= 3:
-                    all_combos.append(Match((i, j - counter), counter, 'h', prev_letter))
-                prev_letter = row[j]
-                counter = 1
-            if j == columns - 1 and counter >= 3:
-                all_combos.append(Match((i, j - counter + 1), counter, 'h', prev_letter))
-    for j in range(columns):
-        counter = 1
-        prev_letter = '*'
-        for i in range(rows):
-            if board[i][j] == ' ':
-                continue
-            if prev_letter == board[i][j]:
-                counter += 1
-            else:
-                if counter >= 3:
-                    all_combos.append(Match((i - counter, j), counter, 'v', prev_letter))
-                prev_letter = board[i][j]
-                counter = 1
-            if i == rows - 1 and counter >= 3:
-                all_combos.append(Match((i - counter + 1, j), counter, 'v', prev_letter))
+    cc_list = []  # number of combos per iteration
+    guard_breaks = 0 # number of guard break activations
 
-    # print('Roots are: ' + str([combo.get_roots() for combo in all_combos]))
-    to_purge = set()
-    for i in range(len(all_combos) - 1):
-        for j in range(i + 1, len(all_combos)):
-            if all_combos[i].att == all_combos[j].att and all_combos[i] in all_combos[j]:
-                all_combos[i].swallow(all_combos[j])
-                to_purge.add(j)
-    for i, j in enumerate(sorted(to_purge)):
-        all_combos.pop(j - i)
-    return [combo.get_coord() for combo in all_combos]
+    for i in range(ITERATIONS+1):
+        board = copy.deepcopy(perm_board)
+        total_combos = 0
+        guard_break = False
+        combo_coords = find_combos(board)
 
+        while len(combo_coords) > 0:
+            total_combos += len(combo_coords)
+            remove_combos(board, combo_coords)
+            fall_all(board)
+            if cc_list:
+                create_skyfalls(board)
+            combo_coords = find_combos(board)
+
+        cc_list.append(total_combos)
+
+    base_combo_count = cc_list[0]
+    cc_list = cc_list[1:]
+
+    if not skyfall:
+        return base_combo_count
+
+    sum_list = [sum(cc_list[i:(i + x_sf[1])]) - x_sf[1] * base_combo_count for i in range(ITERATIONS - x_sf[1])]
+    avg, avg_sf, min_c, p = (np.mean(cc_list), np.mean(cc_list) - base_combo_count, min(cc_list),
+                             sum(x > x_sf[0] for x in sum_list) / ITERATIONS)
+
+    if print_stats:
+        print(f'board was {board_string}')
+        print(f'The mean combos is {avg}')
+        print(f'The mean number of skyfalls is {avg_sf}')
+        print(f'The min number of combos is {min_c}')
+        print(f'The observed probability of getting {x_sf[0]} skyfalls over {x_sf[1]} turns is {p}')
+
+    return board_string, avg, avg_sf, min_c, p
 
 def set_skyfalls(distribution: [int]):
     '''sets the skyfalls in order of RBGLDHJP'''
     global sf_distribution
     sf_distribution = distribution
 
-
-def calculate_base_combo(board: [[str]]):
-    total_combos = 0
-    combo_coords = find_nosf_combos(board)
-    while len(combo_coords) > 0:
-        total_combos += len(combo_coords)
-        remove_combos(board, combo_coords, skyfall=False)
-        combo_coords = find_nosf_combos(board)
-
-    return total_combos
-
-
-def combo_sim(board_string: str, board_size: (0, 1), x_sf: (int, int), print_stats=True, skyfall=True) -> \
-        (str, int, int, int, int):
-    '''board size is 0 for 6x5 and 1 for 7x6;
-    x_sf is how many skyfalls you want over how many turns'''
+def set_to_76():
     global rows
     global columns
-    if board_size == 1:
-        rows = 6
-        columns = 7
-
-    assert len(board_string) == rows * columns
-    perm_board = [[board_string[i * columns + j] for j in range(columns)] for i in range(rows)]
-
-    # no skyfall combo count
-    base_combo_count = calculate_base_combo(copy.deepcopy(perm_board))
-
-    cc_list = []  # number of combos per iteration
-    guard_break_list = []
-
-    if skyfall:
-
-        for i in range(ITERATIONS):
-
-            board = copy.deepcopy(perm_board)
-
-            total_combos = 0
-            guard_break = False
-            combo_coords = find_combos(board)
-
-            while len(combo_coords) > 0:
-                total_combos += len(combo_coords)
-                remove_combos(board, combo_coords)
-                combo_coords = find_combos(board)
-
-            cc_list.append(total_combos)
-
-        sum_list = [sum(cc_list[i:(i + x_sf[1])]) - x_sf[1] * base_combo_count for i in range(ITERATIONS - x_sf[1])]
-        avg, avg_sf, min_c, p = (np.mean(cc_list), np.mean(cc_list) - base_combo_count, min(cc_list),
-                                 sum(x > x_sf[0] for x in sum_list) / ITERATIONS)
-        if print_stats:
-            print(f'board was {board_string}')
-            print(f'The mean combos is {avg}')
-            print(f'The mean number of skyfalls is {avg_sf}')
-            print(f'The min number of combos is {min_c}')
-            print(f'The observed probability of getting {x_sf[0]} skyfalls over {x_sf[1]} turns is {p}')
-
-        return board_string, avg, avg_sf, min_c, p
-
-    else:
-        if print_stats:
-            print(f'The combo count is {base_combo_count}')
-        return base_combo_count
-
+    rows = 6
+    columns = 7
 
 if __name__ == '__main__':
     board_string = "DDDLLLLLLDDDDDDLLLLLLDDDDDDLLL"
-
-    combo_sim(board_string, 0, (15, 2), print_stats=True, skyfall=True)
+    combo_sim(board_string, (15, 2), print_stats=True, skyfall=True)
